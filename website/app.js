26b94fbd2b5ba8feaa6be48c94afdf7d102d89b4
Ahmed Elbarbary <98464590+ibarbary@users.noreply.github.com>
2022-03-19 18:32:51 +0200

    Add files via upload

---
+++ website/app.js
@@ -0,0 +1,68 @@
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear();
+
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+
+const apiKey = "&appid=a462a2394c4192ac38d1e5bda213dae0&units=imperial";
+
+
+document.querySelector('#generate').addEventListener('click', generateWeather);
+
+function generateWeather()
+{
+  const zip = document.querySelector('#zip').value;
+  const feelings = document.querySelector("#feelings").value;
+
+  getWeather(zip)
+  .then(function(data){
+    postData('/add', {
+      date: newDate,
+      city: data.name,
+      temp: data.main.temp,
+      feel: feelings,
+    })
+  })
+  .then(retrieveData())
+}
+
+const getWeather = async (zip)=>{
+  const res = await fetch(baseURL + zip + apiKey);
+
+  try{
+  const data = await res.json();
+  console.log(data);
+  return data;
+  }catch(error){
+    console.log("error", error);
+  }
+}
+
+const postData = async (url='', data={}) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+};
+
+const retrieveData = async () =>{
+ const request = await fetch('/all');
+ try {
+ // Transform into JSON
+ const allData = await request.json()
+ console.log(allData)
+ // Write updated data to DOM elements
+ document.getElementById('temp').innerHTML = Math.round(allData.temp)+ ' deg';
+ document.getElementById("content").innerHTML = `" ${allData.feel} "`;
+ document.getElementById('date').innerHTML = allData.date;
+ document.getElementById("city").innerHTML = allData.city;
+ }
+ catch(error) {
+   console.log("error", error);
+   // appropriately handle the error
+ }
+}
